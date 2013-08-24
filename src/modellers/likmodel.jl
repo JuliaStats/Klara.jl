d0bd9187fc97989ff920362a946d4457a5b8b620
theodore <theodore.papamarkou@gmail.com>
2013-08-24 10:35:58 +0100

    Added tensor field in MCMCLikelihoodModel (i.e. derivatives of Hessian, needed for geometric MCMC samplers)

--- src/modellers/likmodel.jl
+++ src/modellers/likmodel.jl
@@ -15,6 +15,7 @@ type MCMCLikelihoodModel <: MCMCModel
 	eval::Function                 # log-likelihood evaluation function
 	evalg::Union(Nothing,Function) # 2-tuple (log-lik, gradient vector) evaluation function
 	evalh::Union(Nothing,Function) # 3-tuple (log-lik, gradient vector, hessian) evaluation function
+	evalt::Union(Nothing,Function) # 4-tuple (log-lik, gradient vector, hessian, tensor) evaluation function
 	pmap::PMap                     # map to/from parameter vector from/to user-friendly variables
 	size::Integer                  # parameter vector size
 	init::Vector{Float64}          # parameter vector initial values
@@ -23,6 +24,7 @@ type MCMCLikelihoodModel <: MCMCModel
 	function MCMCLikelihoodModel(	f::Function, 
 									g::Union(Nothing, Function), 
 									h::Union(Nothing, Function),
+									t::Union(Nothing, Function),
 									i::Vector{Float64}, sc::Vector{Float64}, pmap::PMap)
 		s = size(i,1)
 
@@ -37,10 +39,13 @@ type MCMCLikelihoodModel <: MCMCModel
 		# check that hessian function can be called with a vector of Float64 as argument
 		assert(h == nothing | hasvectormethod(h), "hessian function cannot be called with Vector{Float64}")
 
+		# check that hessian function can be called with a vector of Float64 as argument
+		assert(t == nothing | hasvectormethod(t), "tensor function cannot be called with Vector{Float64}")
+
 		# check that initial values are in the support of likelihood function
 		assert(f(i) != -Inf, "Initial values out of model support, try other values")
 
-		new(f, g, h, pmap, s, i, sc)
+		new(f, g, h, t, pmap, s, i, sc)
 	end
 end
 
@@ -48,14 +53,17 @@ typealias MCMCLikModel MCMCLikelihoodModel
 
 # Model creation : no gradient or hessian
 MCMCLikelihoodModel( lik::Function; args...) = 
-	MCMCLikelihoodModel(lik, nothing, nothing; args...)
+	MCMCLikelihoodModel(lik, nothing, nothing, nothing; args...)
 MCMCLikelihoodModel( lik::Function, grad::Function; args...) = 
-	MCMCLikelihoodModel(lik, grad, nothing; args...)
+	MCMCLikelihoodModel(lik, grad, nothing, nothing; args...)
+MCMCLikelihoodModel( lik::Function, grad::Function, hessian::Function; args...) = 
+	MCMCLikelihoodModel(lik, grad, hessian, nothing; args...)
 
-# Model creation : gradient+hessian version 
+# Model creation : gradient+hessian+tensor version 
 function MCMCLikelihoodModel(	lik::Function, 
 								grad::Union(Nothing, Function), 
-								hessian::Union(Nothing, Function); 
+								hessian::Union(Nothing, Function),
+								tensor::Union(Nothing, Function); 
 								init::Union(Real, Vector{Float64}) = [1.0], 
 								scale::Union(Real, Vector{Float64}) = 1.0,
 								pmap::Union(Nothing, PMap) = nothing) 
@@ -65,7 +73,7 @@ function MCMCLikelihoodModel(	lik::Function,
 
 	pmap = pmap == nothing ? Dict([:pars], [PDims(1, size(init))]) : pmap # all parameters named "pars"
 
-	MCMCLikelihoodModel(lik, grad, hessian, init, scale, pmap)
+	MCMCLikelihoodModel(lik, grad, hessian, tensor, init, scale, pmap)
 end
 
 # Model creation using expression parsing and autodiff
