fba0f5616cd97bf4585ab141d43b85089aab1842
theodore <theodore.papamarkou@gmail.com>
2013-08-24 11:51:58 +0100

    Started porting RMHMC

--- src/samplers/samplers.jl
+++ src/samplers/samplers.jl
@@ -14,4 +14,7 @@ immutable MCMCSample
 	oldbeta::Vector{Float64}  # previous vector
 	oldll::Float64            # previous log likelihood	
 end
-# TODO : add diagnostic field to MCMCSample
+# TODO 1 : add diagnostic field to MCMCSample
+# TODO 2 : change beta to pars, oldbeta to oldpars (and perhaps ll to logtarget, oldll to oldlogtarget.
+# The reason target seems a preferable name over lik is because the target can be either the posterior
+# in a Bayesian context or just a distribution (likelihood) if we don't want to use a proper prior in our model)
