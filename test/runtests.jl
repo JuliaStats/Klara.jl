ed4851f186719ddb2203a3f9b6f0496828d74269
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2014-10-26 21:18:35 +0000

    Updated test file for slice sampler

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,13 @@
-my_tests = ["test_empmctuner.jl",
-            "test_ARS1.jl",
-            "test_ARS2.jl"]
+tests =
+  ["test_empmctuner",
+  "test_ARS1",
+  "test_ARS2",
+  "test_slice_sampler"]
 
 println("Running tests:")
 
-for my_test in my_tests
-    println("  * $(my_test) *")
-    include(my_test)
+for t in tests
+  tfile = t*".jl"
+  println("  * $(tfile) *")
+  include(tfile)
 end
