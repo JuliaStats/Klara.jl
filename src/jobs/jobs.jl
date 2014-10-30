c232bbb6e8d0a0ca955589e89117f8c9811168fd
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2014-10-30 21:50:04 +0000

    Up and running again in Jv0.4

--- src/jobs/jobs.jl
+++ src/jobs/jobs.jl
@@ -1,25 +1,32 @@
 ### Constructors for setting up a Monte Carlo job
 
-function MCJob{M<:MCModel, S<:MCSampler, T<:MCTuner}(m::M, s::S, r::SerialMC;
-  tuner::T=VanillaMCTuner(), job::Symbol=:task)
+function MCJob{M<:MCModel, S<:MCSampler, T<:MCTuner}(m::M, s::S, r::SerialMC, t::T, j::Symbol)
   mcjob::MCJob
-  if job == :plain
-    mcjob = PlainMCJob(m, s, r, tuner)
-  elseif job == :task
-    mcjob = TaskMCJob(m, s, r, tuner)
+  if j == :plain
+    mcjob = PlainMCJob(m, s, r, t)
+  elseif j == :task
+    mcjob = TaskMCJob(m, s, r, t)
   else
     error("Only :plain and :task jobs are available.")
   end
   mcjob
 end
 
+MCJob{M<:MCModel, S<:MCSampler, T<:MCTuner}(m::M, s::S, r::SerialMC; tuner::T=VanillaMCTuner(), job::Symbol=:task) =
+  MCJob(m, s, r, tuner, job)
+
 ### Constructors for setting up a vector of Monte Carlo jobs
 
-function MCJob{M<:MCModel, S<:MCSampler, T<:MCTuner}(m::Vector{M}, s::Vector{S}, r::Vector{SerialMC};
-  tuners::Vector{T}=fill(VanillaMCTuner(), length(m)), jobs::Vector{Symbol}=fill(:task, length(m)))
-  @assert length(m) == length(s) == length(r) == length(t) "Number of models, samplers, runners and tuners not equal."
-  map(MCJob, m, s, r, t)
+function MCJob{M<:MCModel, S<:MCSampler, T<:MCTuner}(m::Vector{M}, s::Vector{S}, r::Vector{SerialMC}, t::Vector{T},
+  j::Vector{Symbol})
+  @assert length(m) == length(s) == length(r) == length(t) == length(j)
+    "Number of models, samplers, runners, tuners and job types not equal."
+  map(MCJob, m, s, r, t, j)
 end
+
+MCJob{M<:MCModel, S<:MCSampler, T<:MCTuner}(m::Vector{M}, s::Vector{S}, r::Vector{SerialMC};
+  tuners::Vector{T}=fill(VanillaMCTuner(), length(m)), jobs::Vector{Symbol}=fill(:task, length(m))) =
+  map(MCJob, m, s, r, tuners, jobs)
 
 ### Functions for running jobs
 
