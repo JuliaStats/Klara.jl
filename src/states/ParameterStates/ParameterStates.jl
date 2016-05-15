a99b3e4cd34e461297767a913a743215cdcf77db
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2016-05-15 22:10:45 +0100

    Added sphinx doc for basic parameter states

--- src/states/ParameterStates/ParameterStates.jl
+++ src/states/ParameterStates/ParameterStates.jl
@@ -10,6 +10,8 @@ typealias ParameterStateVector{S<:ParameterState} Vector{S}
 value_support{S<:ValueSupport, F<:VariateForm}(::Type{ParameterState{S, F}}) = S
 variate_form{S<:ValueSupport, F<:VariateForm}(::Type{ParameterState{S, F}}) = F
 
+diagnostics(state::ParameterState) = Dict(zip(state.diagnostickeys, state.diagnosticvalues))
+
 Base.(:(==)){S<:ParameterState}(z::S, w::S) = reduce(&, [getfield(z, n) == getfield(w, n) for n in fieldnames(S)])
 
 Base.isequal{S<:ParameterState}(z::S, w::S) = reduce(&, [isequal(getfield(z, n), getfield(w, n)) for n in fieldnames(S)])
