83110fead4be1e5231764968079ea43916105d8f
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2017-03-27 12:25:20 +0100

    Added several autodiff example scripts

---
+++ doc/examples/swiss/HMC/noadaptation/reversediff.jl
@@ -0,0 +1,44 @@
+using Klara
+
+covariates, = dataset("swiss", "measurements")
+ndata, npars = size(covariates)
+
+covariates = (covariates.-mean(covariates, 1))./repmat(std(covariates, 1), ndata, 1)
+
+outcome, = dataset("swiss", "status")
+outcome = vec(outcome)
+
+function ploglikelihood(p, v)
+  Xp = v[2]*p
+  dot(Xp, v[3])-sum(log(1+exp(Xp)))
+end
+
+plogprior(p, v) = -0.5*(dot(p, p)/v[1]+length(p)*log(2*pi*v[1]))
+
+p = BasicContMuvParameter(
+  :p,
+  loglikelihood=ploglikelihood,
+  logprior=plogprior,
+  nkeys=4,
+  diffopts=DiffOptions(mode=:reverse)
+)
+
+model = likelihood_model([Hyperparameter(:λ), Data(:X), Data(:y), p], isindexed=false)
+
+sampler = HMC(0.35)
+
+mcrange = BasicMCRange(nsteps=10000, burnin=1000)
+
+v0 = Dict(:λ=>100., :X=>covariates, :y=>outcome, :p=>[5.1, -0.9, 8.2, -4.5])
+
+outopts = Dict{Symbol, Any}(:monitor=>[:value, :logtarget, :gradlogtarget], :diagnostics=>[:accept])
+
+job = BasicMCJob(model, sampler, mcrange, v0, outopts=outopts)
+
+run(job)
+
+chain = output(job)
+
+mean(chain)
+
+acceptance(chain)
