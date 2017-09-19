4855ee6dd4ccc8098eb9c92966ebaa6cd5272a01
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2017-09-19 01:26:32 +0100

    Eliminated code generation from iterate() method of RAM sampler

--- src/samplers/iterate/RAM.jl
+++ src/samplers/iterate/RAM.jl
@@ -1,110 +1,130 @@
-function codegen(::Type{Val{:iterate}}, job::BasicMCJob, ::Type{RAM})
-  local result::Expr
-  update = []
-  noupdate = []
-  body = []
-  local dindex::Integer
-
-  vform = variate_form(job.pstate)
-  if vform != Univariate && vform != Multivariate
-    error("Only univariate or multivariate parameter states allowed in MH code generation")
-  end
-
-  push!(body, :(_job.sstate.count += 1))
+function iterate!(job::BasicMCJob, ::Type{RAM}, ::Type{Univariate})
+  job.sstate.count += 1
 
   if job.tuner.verbose
-    push!(body, :(_job.sstate.tune.proposed += 1))
+    job.sstate.tune.proposed += 1
   end
 
-  if vform == Univariate
-    push!(body, :(_job.sstate.randnsample = randn()))
-    push!(body, :(_job.sstate.pstate.value = _job.pstate.value+_job.sstate.S*_job.sstate.randnsample))
-  elseif vform == Multivariate
-    push!(body, :(_job.sstate.randnsample[:] = randn(_job.pstate.size)))
-    push!(body, :(_job.sstate.pstate.value[:] = _job.pstate.value+_job.sstate.S*_job.sstate.randnsample))
-  end
+  job.sstate.randnsample = randn()
+  job.sstate.pstate.value = job.pstate.value+job.sstate.S*job.sstate.randnsample
 
-  push!(body, :(_job.parameter.logtarget!(_job.sstate.pstate)))
+  job.parameter.logtarget!(job.sstate.pstate)
 
-  push!(body, :(_job.sstate.ratio = _job.sstate.pstate.logtarget-_job.pstate.logtarget))
+  job.sstate.ratio = job.sstate.pstate.logtarget-job.pstate.logtarget
 
-  if vform == Univariate
-    push!(update, :(_job.pstate.value = _job.sstate.pstate.value))
-  elseif vform == Multivariate
-    push!(update, :(_job.pstate.value = copy(_job.sstate.pstate.value)))
-  end
-  push!(update, :(_job.pstate.logtarget = _job.sstate.pstate.logtarget))
-  if in(:loglikelihood, job.outopts[:monitor]) && job.parameter.loglikelihood! != nothing
-    push!(update, :(_job.pstate.loglikelihood = _job.sstate.pstate.loglikelihood))
-  end
-  if in(:logprior, job.outopts[:monitor]) && job.parameter.logprior! != nothing
-    push!(update, :(_job.pstate.logprior = _job.sstate.pstate.logprior))
-  end
-  dindex = findfirst(job.outopts[:diagnostics], :accept)
-  if dindex != 0
-    push!(update, :(_job.pstate.diagnosticvalues[$dindex] = true))
-    push!(noupdate, :(_job.pstate.diagnosticvalues[$dindex] = false))
-  end
-  if job.tuner.verbose
-    push!(update, :(_job.sstate.tune.accepted += 1))
-  end
+  if (job.sstate.ratio > 0 || (job.sstate.ratio > log(rand())))
+    job.pstate.value = job.sstate.pstate.value
 
-  push!(body, Expr(:if, :(_job.sstate.ratio > 0 || (_job.sstate.ratio > log(rand()))), Expr(:block, update...), noupdate...))
+    job.pstate.logtarget = job.sstate.pstate.logtarget
+    if in(:loglikelihood, job.outopts[:monitor]) && job.parameter.loglikelihood! != nothing
+      job.pstate.loglikelihood = job.sstate.pstate.loglikelihood
+    end
+    if in(:logprior, job.outopts[:monitor]) && job.parameter.logprior! != nothing
+      job.pstate.logprior = job.sstate.pstate.logprior
+    end
 
-  if job.tuner.verbose
-    fmt_iter = format_iteration(ndigits(job.range.burnin))
-    fmt_perc = format_percentage()
-
-    push!(body, :(
-      if _job.sstate.tune.totproposed <= _job.range.burnin && mod(_job.sstate.tune.proposed, _job.tuner.period) == 0
-        rate!(_job.sstate.tune)
-        println(
-          "Burnin iteration ",
-          $(fmt_iter)(_job.sstate.tune.totproposed),
-          " of ",
-          _job.range.burnin,
-          ": ",
-          $(fmt_perc)(100*_job.sstate.tune.rate),
-          " % acceptance rate"
-        )
-        reset_burnin!(_job.sstate.tune)
+    if !isempty(job.sstate.diagnosticindices)
+      if haskey(job.sstate.diagnosticindices, :accept)
+        job.pstate.diagnosticvalues[job.sstate.diagnosticindices[:accept]] = true
       end
-    ))
-  end
+    end
 
-  if vform == Univariate
-    push!(body, :(_job.sstate.η = min(1, _job.sstate.count^(-_job.sampler.γ))))
-
-    push!(body, :(_job.sstate.SST = _job.sstate.η*(min(1, exp(_job.sstate.ratio))-_job.sampler.targetrate)))
-
-    push!(body, :(_job.sstate.SST = abs2(_job.sstate.S)*(1+_job.sstate.SST)))
-
-    push!(body, :(_job.sstate.S = chol(_job.sstate.SST)))
-  elseif vform == Multivariate
-    push!(body, :(_job.sstate.η = min(1, _job.pstate.size*_job.sstate.count^(-_job.sampler.γ))))
-
-    push!(
-      body,
-      :(
-        _job.sstate.SST[:, :] = (
-          _job.sstate.randnsample*_job.sstate.randnsample'/dot(_job.sstate.randnsample, _job.sstate.randnsample)*
-          _job.sstate.η*(min(1, exp(_job.sstate.ratio))-_job.sampler.targetrate)
-        )
-      )
-    )
-
-    push!(body, :(_job.sstate.SST[:, :] = _job.sstate.S*(eye(_job.pstate.size)+_job.sstate.SST)*_job.sstate.S'))
-
-    push!(body, :(_job.sstate.S[:, :] = ctranspose(chol(Hermitian(_job.sstate.SST)))))
-  end
-
-  @gensym _iterate
-
-  result = quote
-    function $_iterate(_job::BasicMCJob)
-      $(body...)
+    if job.tuner.verbose
+      job.sstate.tune.accepted += 1
+    end
+  else
+    if !isempty(job.sstate.diagnosticindices)
+      if haskey(job.sstate.diagnosticindices, :accept)
+        job.pstate.diagnosticvalues[job.sstate.diagnosticindices[:accept]] = false
+      end
     end
   end
 
-  result
+  if job.tuner.verbose
+    if job.sstate.tune.totproposed <= job.range.burnin && mod(job.sstate.tune.proposed, job.tuner.period) == 0
+      rate!(job.sstate.tune)
+      println(
+        "Burnin iteration ",
+        job.fmt_iter(job.sstate.tune.totproposed),
+        " of ",
+        job.range.burnin,
+        ": ",
+        job.fmt_perc(100*job.sstate.tune.rate),
+        " % acceptance rate"
+      )
+      reset_burnin!(job.sstate.tune)
+    end
+  end
+
+  job.sstate.η = min(1, job.sstate.count^(-job.sampler.γ))
+  job.sstate.SST = job.sstate.η*(min(1, exp(job.sstate.ratio))-job.sampler.targetrate)
+  job.sstate.SST = abs2(job.sstate.S)*(1+job.sstate.SST)
+  job.sstate.S = chol(job.sstate.SST)
+end
+
+function iterate!(job::BasicMCJob, ::Type{RAM}, ::Type{Multivariate})
+  job.sstate.count += 1
+
+  if job.tuner.verbose
+    job.sstate.tune.proposed += 1
+  end
+
+  job.sstate.randnsample[:] = randn(job.pstate.size)
+  job.sstate.pstate.value[:] = job.pstate.value+job.sstate.S*job.sstate.randnsample
+
+  job.parameter.logtarget!(job.sstate.pstate)
+
+  job.sstate.ratio = job.sstate.pstate.logtarget-job.pstate.logtarget
+
+  if (job.sstate.ratio > 0 || (job.sstate.ratio > log(rand())))
+    job.pstate.value = copy(job.sstate.pstate.value)
+
+    job.pstate.logtarget = job.sstate.pstate.logtarget
+    if in(:loglikelihood, job.outopts[:monitor]) && job.parameter.loglikelihood! != nothing
+      job.pstate.loglikelihood = job.sstate.pstate.loglikelihood
+    end
+    if in(:logprior, job.outopts[:monitor]) && job.parameter.logprior! != nothing
+      job.pstate.logprior = job.sstate.pstate.logprior
+    end
+
+    if !isempty(job.sstate.diagnosticindices)
+      if haskey(job.sstate.diagnosticindices, :accept)
+        job.pstate.diagnosticvalues[job.sstate.diagnosticindices[:accept]] = true
+      end
+    end
+
+    if job.tuner.verbose
+      job.sstate.tune.accepted += 1
+    end
+  else
+    if !isempty(job.sstate.diagnosticindices)
+      if haskey(job.sstate.diagnosticindices, :accept)
+        job.pstate.diagnosticvalues[job.sstate.diagnosticindices[:accept]] = false
+      end
+    end
+  end
+
+  if job.tuner.verbose
+    if job.sstate.tune.totproposed <= job.range.burnin && mod(job.sstate.tune.proposed, job.tuner.period) == 0
+      rate!(job.sstate.tune)
+      println(
+        "Burnin iteration ",
+        job.fmt_iter(job.sstate.tune.totproposed),
+        " of ",
+        job.range.burnin,
+        ": ",
+        job.fmt_perc(100*job.sstate.tune.rate),
+        " % acceptance rate"
+      )
+      reset_burnin!(job.sstate.tune)
+    end
+  end
+
+  job.sstate.η = min(1, job.pstate.size*job.sstate.count^(-job.sampler.γ))
+  job.sstate.SST[:, :] = (
+    job.sstate.randnsample*job.sstate.randnsample'/dot(job.sstate.randnsample, job.sstate.randnsample)*
+    job.sstate.η*(min(1, exp(job.sstate.ratio))-job.sampler.targetrate)
+  )
+  job.sstate.SST[:, :] = job.sstate.S*(eye(job.pstate.size)+job.sstate.SST)*job.sstate.S'
+  job.sstate.S[:, :] = ctranspose(chol(Hermitian(job.sstate.SST)))
 end
