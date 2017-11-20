7ce4796aab1c53ae2cb6afada9dcdcc264777115
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2017-11-20 07:40:15 +0000

    Added generate_logtarget() for multivariate parameters

--- src/factors/GibbsFactor.jl
+++ src/factors/GibbsFactor.jl
@@ -173,7 +173,7 @@ function generate_logtarget(
     stream,
     string(
       "function logtarget_",
-      f.variables[1],
+      f.variables[i],
       "(_state::",
       default_state_type(f.variabletypes[i]),
       "_states::VariableStateVector)\n")
@@ -187,97 +187,61 @@ function generate_logtarget(
 end
 
 function generate_logtarget(
+  f::GibbsFactor, i::Integer, stream::IOStream, ::Type{Multivariate}, nc::Integer=num_cliques(f), nv::Integer=num_vertices(f)
+)
+  local body = ["_state.logtarget = 0."]
+  local lpargs::Vector{String}
+
+  for j in 1:nc
+    if in(f.variables[i], f.cliques[j])
+      lpargs = []
+
+      for v in f.cliques[j]
+        if v == f.variables[i]
+          push!(lpargs, "_state.value")
+        else
+          push!(lpargs, "_states[$(f.ofvariable[v])].value")
+        end
+      end
+
+      push!(body, :(_state.logtarget += f.logpotentials[$j]($(lpargs...))))
+      push!(body, "_state.logtarget += f.logpotentials[$j]($(join(lpargs, ", ")))")
+    end
+  end
+
+  write(
+    stream,
+    string(
+      "function logtarget_",
+      f.variables[i],
+      "(_state::",
+      default_state_type(f.variabletypes[i]),
+      "_states::VariableStateVector)\n")
+  )
+
+  for ln in body
+    write(stream, "  $(ln)\n")
+  end
+
+  write(stream, "end\n")
+end
+
+# TODO 1: Add generate_logtarget() without vform input argument (to be inferred)
+# TODO 2: Modify generate_logtarget() below to operate without vform input argument
+# TODO 3: Allow to optionally add empty line between file-generated chunks of code
+
+function generate_logtarget(
   f::GibbsFactor,
   i::Integer,
   filename::AbstractString,
-  vform::Type{Univariate},
+  vform::Type{F};
+  mode::AbstractString="w",
   nc::Integer=num_cliques(f),
   nv::Integer=num_vertices(f)
-)
-  stream = open(filename, "w")
+) where F<:VariateForm
+  stream = open(filename, mode)
   generate_logtarget(f, i, stream, vform, nc, nv)
   close(stream)
-end
-
-function codegen_logtarget(
-  f::GibbsFactor, i::Integer, ::Type{Univariate}, nc::Integer=num_cliques(f), nv::Integer=num_vertices(f)
-)
-  local body = [:(_state.logtarget = 0.)]
-  local lpargs::Vector{Expr}
-  local lptargs::Vector{Int8} = fill(Int8(0), nv)
-
-  for j in 1:nc
-    if in(f.variables[i], f.cliques[j])
-      lpargs = []
-
-      for v in f.cliques[j]
-        if v == f.variables[i]
-          if f.support[f.ofvariable[v]] == Pair(0., Inf) && f.reparametrize[f.ofvariable[v]] == :log
-            push!(lpargs, :(exp(_state.value)))
-            lptargs[f.ofvariable[v]] = Int8(1)
-          else
-            push!(lpargs, :(_state.value))
-          end
-        else
-          if f.support[f.ofvariable[v]] == Pair(0., Inf) && f.reparametrize[f.ofvariable[v]] == :log
-            push!(lpargs, :(exp(_states[$(f.ofvariable[v])].value)))
-            lptargs[f.ofvariable[v]] = Int8(2)
-          else
-            push!(lpargs, :(_states[$(f.ofvariable[v])].value))
-          end
-        end
-      end
-
-      push!(body, :(_state.logtarget += f.logpotentials[$j]($(lpargs...))))
-    end
-  end
-
-  for k in 1:nv
-    if lptargs[k] == 1
-      push!(body, :(_state.logtarget += _state.value))
-    elseif lptargs[k] == 2
-      push!(body, :(_state.logtarget += _states[$k].value))
-    end
-  end
-
-  @gensym logtarget
-
-  quote
-    function $logtarget(_state::$(default_state_type(f.variabletypes[i])), _states::VariableStateVector)
-      $(body...)
-    end
-  end
-end
-
-function codegen_logtarget(
-  f::GibbsFactor, i::Integer, ::Type{Multivariate}, nc::Integer=num_cliques(f), nv::Integer=num_vertices(f)
-)
-  local body = [:(_state.logtarget = 0.)]
-  local lpargs::Vector{Expr}
-
-  for j in 1:nc
-    if in(f.variables[i], f.cliques[j])
-      lpargs = []
-
-      for v in f.cliques[j]
-        if v == f.variables[i]
-          push!(lpargs, :(_state.value))
-        else
-          push!(lpargs, :(_states[$(f.ofvariable[v])].value))
-        end
-      end
-
-      push!(body, :(_state.logtarget += f.logpotentials[$j]($(lpargs...))))
-    end
-  end
-
-  @gensym logtarget
-
-  quote
-    function $logtarget(_state::$(default_state_type(f.variabletypes[i])), _states::VariableStateVector)
-      $(body...)
-    end
-  end
 end
 
 function codegen_transform(f::GibbsFactor, i::Integer, nt::Integer=num_transforms(f))
